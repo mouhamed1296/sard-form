d4845b4033b7b18c07da2746414ae662d506f055
mbayelel@gmail.com <mbayelel@gmail.com>
2021-11-08 01:25:34 +0100

    Bug fixed

--- src/components/GenerateLabel.js
+++ src/components/GenerateLabel.js
@@ -4,12 +4,12 @@ const GenerateLabel = ({labelProps, children, fieldId}) => {
     let label = null
 
     if (labelProps.content || labelProps.className || labelProps.style) {
-        label = <label htmlFor={fieldProps.id} className={labelProps.className} style={labelProps.style}>
+        label = <label htmlFor={fieldId} className={labelProps.className} style={labelProps.style}>
             {labelProps.content}
             {children}
         </label>
     } else {
-        label = <label htmlFor={fieldProps.id}>
+        label = <label htmlFor={fieldId}>
             {labelProps}
             {children}
         </label>
